b5eb88609eb313016ff7971a14e25604a7361db3
spiderocta <cryptofer@tuta.io>
2022-10-21 09:19:55 +0200

    initial commit

---
+++ src/main/java/com/scool/ScoolApplication.java
@@ -0,0 +1,13 @@
+package com.scool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scool/ScoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
