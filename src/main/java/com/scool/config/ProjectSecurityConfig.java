0c4e8683018634e29ce12829d0286a74bdd3d471
spiderocta <cryptofer@tuta.io>
2022-10-25 11:47:00 +0200

    in memory authentication

--- src/main/java/com/scool/config/ProjectSecurityConfig.java
+++ src/main/java/com/scool/config/ProjectSecurityConfig.java
@@ -18,13 +18,19 @@ public class ProjectSecurityConfig {
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests()
+                .mvcMatchers("/dashboard").authenticated()
                 .mvcMatchers("/home").permitAll()
                 .mvcMatchers("/holidays/**").permitAll()
                 .mvcMatchers("/contact").permitAll()
                 .mvcMatchers("/saveMsg").permitAll()
-                .mvcMatchers("/courses").authenticated()
+                .mvcMatchers("/courses").permitAll()
                 .mvcMatchers("/about").permitAll()
-                .and().formLogin().and().httpBasic();
+                .mvcMatchers("/login").permitAll()
+                .and().formLogin().loginPage("/login")
+                .defaultSuccessUrl("/dashboard").failureUrl("/login?error=true").permitAll()
+                .and().logout().logoutSuccessUrl("/login?logout=true").invalidateHttpSession(true).permitAll()
+                .and().httpBasic();
+
         return http.build();
     }
 
