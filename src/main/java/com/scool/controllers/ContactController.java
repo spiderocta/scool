9a884e8cee4183f6e2e3f53e1f79ec293c68207b
spiderocta <cryptofer@tuta.io>
2022-10-26 17:53:12 +0200

    JDBC Templates

--- src/main/java/com/scool/controllers/ContactController.java
+++ src/main/java/com/scool/controllers/ContactController.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -15,33 +14,54 @@ import com.scool.model.Contact;
 import com.scool.service.ContactService;
 
 import javax.validation.Valid;
+import org.springframework.security.core.Authentication;
 
-@Controller
+
+import java.util.List;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
+
 @Slf4j
+@Controller
 public class ContactController {
-	
+
 	private final ContactService contactService;
-	
+
 	@Autowired
 	public ContactController(ContactService contactService) {
 		this.contactService = contactService;
 	}
-	
-	
-	
+
 	@RequestMapping("/contact")
-	public String displayContact(Model model) {
+	public String displayContactPage(Model model) {
 		model.addAttribute("contact", new Contact());
-		return "contact";
+		return "contact.html";
 	}
 
-	@PostMapping("/saveMsg")
+
+	@RequestMapping(value = "/saveMsg",method = POST)
 	public String saveMessage(@Valid @ModelAttribute("contact") Contact contact, Errors errors) {
 		if(errors.hasErrors()){
-			log.error("contact form validations failed due to : " + errors.toString());
-			return "contact";
+			log.error("Contact form validation failed due to : " + errors.toString());
+			return "contact.html";
 		}
-		contactService.saveMessageDetaisl(contact);
+		contactService.saveMessageDetails(contact);
 		return "redirect:/contact";
 	}
+
+	@RequestMapping("/displayMessages")
+	public ModelAndView displayMessages(Model model) {
+		List<Contact> contactMsgs = contactService.findMsgsWithOpenStatus();
+		ModelAndView modelAndView = new ModelAndView("messages.html");
+		modelAndView.addObject("contactMsgs",contactMsgs);
+		return modelAndView;
+	}
+
+	@RequestMapping(value = "/closeMsg",method = GET)
+	public String closeMsg(@RequestParam int id, Authentication authentication) {
+		contactService.updateMsgStatus(id,authentication.getName());
+		return "redirect:/displayMessages";
+	}
+
 }
--- src/main/java/com/scool/service/ContactService.java
+++ src/main/java/com/scool/service/ContactService.java
@@ -1,18 +1,48 @@
 package com.scool.service;
 
+import com.scool.constants.ScoolConstants;
+import com.scool.repository.ContactRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.scool.model.Contact;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Slf4j
 @Service
 public class ContactService {
-	
-	public boolean saveMessageDetaisl(Contact contact) {
-		boolean isSaved = true;
-		
-		//logic to persist the data into the database
-		log.info(contact.toString());
+
+	@Autowired
+	private ContactRepository contactRepository;
+
+
+	public boolean saveMessageDetails(Contact contact){
+		boolean isSaved = false;
+		contact.setStatus(ScoolConstants.OPEN);
+		contact.setCreatedBy(ScoolConstants.ANONYMOUS);
+		contact.setCreatedAt(LocalDateTime.now());
+		int result = contactRepository.saveContactMsg(contact);
+		if(result>0) {
+			isSaved = true;
+		}
 		return isSaved;
 	}
+
+	public List<Contact> findMsgsWithOpenStatus(){
+		List<Contact> contactMsgs = contactRepository.findMsgsWithStatus(ScoolConstants.OPEN);
+		return contactMsgs;
+	}
+
+	public boolean updateMsgStatus(int contactId, String updatedBy){
+		boolean isUpdated = false;
+		int result = contactRepository.updateMsgStatus(contactId,ScoolConstants.CLOSE, updatedBy);
+		if(result>0) {
+			isUpdated = true;
+		}
+		return isUpdated;
+	}
+
 }
